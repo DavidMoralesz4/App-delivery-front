a9a0fd5a5949fe39659ab8261a5d74f003268d3e
Arturo López <jhonas29@gmail.com>
2024-04-03 13:37:54 +0200

    Added logic to the Header component that allows the Search component to be displayed or not depending on the user's role

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom"
 import { useUserContext } from "../../context/UserContext"
+import Search from "./Search";
 
 function Header() {
-
+	const {userData} = useUserContext();
 
 	return (
 		<div className="flex">
@@ -12,6 +13,11 @@ function Header() {
 				</Link> 				
 			</div>
 
+			{
+				userData.userActualRole === "v" || userData.userActualRole === "u"
+					?	<Search handleSubmit={() => console.log("Search Submit")} />
+					: 	null
+			}
 
 			<div className="">
 
