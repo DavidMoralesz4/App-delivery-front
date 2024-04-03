320d6b41a98bbfd6203d45ff4c1294850f1a179a
Arturo López <jhonas29@gmail.com>
2024-04-03 11:56:57 +0200

    Material UI library added to the project

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,20 +3,20 @@ import { Link } from "react-router-dom"
 function Header() {
 	return (
 		<div className="flex">
-				<div className="">
-					<Link to="/" className="">
-						Eatsquality
-					</Link> 				
-				</div>
-
-				<div className="">
-
-				</div>
-
-				<div className="">
-
-				</div>
+			<div className="">
+				<Link to="/" className="">
+					Eatsquality
+				</Link> 				
 			</div>
+
+			<div className="">
+
+			</div>
+
+			<div className="">
+
+			</div>
+		</div>
 	)
 		
 }
